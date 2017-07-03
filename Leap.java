1c081fb61145ed57270dcb6721823c244769629f
Psivaranjani <sathyapalanichamy90@gmail.com>
2017-07-03 21:22:30 +0530

    Create Leap.java

---
+++ Leap.java
@@ -0,0 +1,19 @@
+# efgh
+import java.util.*;
+import java.io.*;
+public class Leap{
+  public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter the year");
+    int a=sc.nextInt();
+    
+    if((a%400==0)||(a%4==0))
+    {
+      System.out.println("leap year");
+    }
+    else
+    {
+      System.out.println("not a leap year");
+    }
+  }
+}
