0e8b929291e6cc8e73c035e614ba57da9a7dec21
Psivaranjani <sathyapalanichamy90@gmail.com>
2017-07-03 21:55:44 +0530

    Create Numofdigit.java

---
+++ Numofdigit.java
@@ -0,0 +1,26 @@
+# efgh
+import java.util.*;
+public class Numofdigit 
+{
+  public static void main(String[] args) 
+  {
+    int n,a=0;
+    Scanner s=new Scanner(System.in);
+    System.out.println("Enter any number : ");
+    n = s.nextInt();
+    if(n<0)
+    {
+      n=n*-1;
+    }
+    else if(n==0)
+    {
+      n=1;
+    }
+    while(n>0)
+    {
+      n=n/10;
+      a++;
+    }
+    System.out.println("Number of Digits in given number is: "+a);
+  }
+}
